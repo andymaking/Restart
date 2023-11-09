f682cb660ed1d5ef283edfa64418d4e7e5fbca7b
Andima Udoh <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>
2023-11-09 14:04:53 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Andima Udoh on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
